fe03ad041b525f8b12d74873d45f069981fd693f
higashi000 <higashi.ict@gmail.com>
2020-02-25 13:31:22 +0900

    Add get target user data

---
+++ slack/userList.go
@@ -0,0 +1,30 @@
+package slack
+
+import (
+	"log"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func UserList(names []string) []slack.User {
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
+
+	users, err := api.GetUsers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var target []slack.User
+
+	for _, e := range users {
+		for _, targetName := range names {
+			if e.RealName == targetName {
+				target = append(target, e)
+				break
+			}
+		}
+	}
+
+	return target
+}
