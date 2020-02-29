317214903b699c5a90a7cc36c101d4332fd8aa7e
higashi000 <higashi.ict@gmail.com>
2020-02-29 15:22:16 +0900

    Remove test function

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 
-	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -31,28 +30,7 @@ func main() {
 	config.AllowAllOrigins = true
 	r.Use(cors.New(config))
 
-	Test(r)
-	TestFirestore(r, client, ctx)
 	addmsg.AddMsg(r, client, ctx)
 
 	r.Run()
-}
-
-func Test(r *gin.Engine) {
-	r.GET("arc/Test", func(c *gin.Context) {
-		c.String(200, "TEST!?!??!?!?!?!??!?!")
-	})
-}
-
-func TestFirestore(r *gin.Engine, client *firestore.Client, ctx context.Context) {
-	r.GET("arc/TestFS", func(c *gin.Context) {
-		_, err := client.Collection("messages").Doc(c.Query("Document")).Set(ctx, map[string]interface{}{
-			"name":    c.Query("name"),
-			"message": c.Query("message"),
-		})
-
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
 }
