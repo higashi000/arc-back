35a199c164a56a06738efb8ef48dbd4833e9822b
higashi000 <higashi.ict@gmail.com>
2020-02-29 16:03:51 +0900

    Fix get body data

--- addmsg/addmsg.go
+++ addmsg/addmsg.go
@@ -3,31 +3,35 @@ package addmsg
 import (
 	"context"
 	"log"
+	"net/http"
 
 	"cloud.google.com/go/firestore"
 	"github.com/gin-gonic/gin"
 )
 
 type Msg struct {
-	Mention []struct {
-		SlackRN   string `json:"slackrn"`
-		TwitterID string `json:"twitterid"`
-	} `json:"mention"`
+	Users []User `json:"mention"`
+	Text  string `json:"text"`
+}
 
-	Text string `json:"text"`
+type User struct {
+	SlackRN   string `json:"slackrn"`
+	TwitterID string `json:"twitterid"`
 }
 
 func AddMsg(r *gin.Engine, client *firestore.Client, ctx context.Context) {
 	r.POST("arc/AddMsg", func(c *gin.Context) {
 		var msg Msg
-		c.BindJSON(msg)
+		c.BindJSON(&msg)
 
 		_, _, err := client.Collection("messages").Add(ctx, map[string]interface{}{
 			"text":    msg.Text,
-			"mention": msg.Mention,
+			"mention": msg.Users,
 		})
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		c.JSON(http.StatusOK, msg)
 	})
 }
