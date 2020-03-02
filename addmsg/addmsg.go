1e3d397e4f91dc0d8403fa575c052890749070c1
higashi000 <higashi.ict@gmail.com>
2020-03-02 16:33:21 +0900

    Add add new message

--- addmsg/addmsg.go
+++ addmsg/addmsg.go
@@ -2,16 +2,19 @@ package addmsg
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 
 	"cloud.google.com/go/firestore"
 	"github.com/gin-gonic/gin"
+	arcslack "github.com/higashi000/arc-back/slack"
 )
 
 type Msg struct {
-	Users []User `json:"mention"`
-	Text  string `json:"text"`
+	Users   []User `json:"mention"`
+	Text    string `json:"text"`
+	Channel string `json:"channel"`
 }
 
 type User struct {
@@ -24,9 +27,24 @@ func AddMsg(r *gin.Engine, client *firestore.Client, ctx context.Context) {
 		var msg Msg
 		c.BindJSON(&msg)
 
-		_, _, err := client.Collection("messages").Add(ctx, map[string]interface{}{
-			"text":    msg.Text,
-			"mention": msg.Users,
+		var slackRN []string
+
+		for _, e := range msg.Users {
+			slackRN = append(slackRN, e.SlackRN)
+		}
+		users := arcslack.UserList(slackRN)
+
+		channelID, ts, err := arcslack.PostMsg(users, msg.Channel, msg.Text)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(channelID, ts)
+
+		_, _, err = client.Collection("messages").Add(ctx, map[string]interface{}{
+			"channelID": channelID,
+			"timestamp": ts,
+			"mention":   msg.Users,
 		})
 		if err != nil {
 			log.Fatal(err)
