c4ff09a71e3c07ebd5a7480036b027aa115bc1f3
higashi000 <higashi.ict@gmail.com>
2020-03-08 21:32:31 +0900

    Change return target user

--- slack/checkReaction.go
+++ slack/checkReaction.go
@@ -2,6 +2,7 @@ package slack
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/slack-go/slack"
 )
@@ -14,47 +15,26 @@ func CheckReaction(ts, channelID string, slackrn []string) ([]string, error) {
 	history, err := api.GetChannelHistory(channelID, slack.HistoryParameters{"0", "0", 50, false, false})
 	if err != nil {
 		fmt.Println(channelID)
-		return reactedUser, err
+		return []string{}, err
 	}
 
-	var targetMsg slack.Message
+	var targetMsg slack.Msg
 
 	for _, e := range history.Messages {
-		if e.Timestamp == ts {
-			targetMsg = e
+		if e.Msg.Timestamp == ts {
+			targetMsg = e.Msg
 			break
 		}
+	}
+
+	if targetMsg.Text == "" {
+		log.Println("error: can not found message")
+		return []string{}, nil
 	}
 
 	for _, e := range targetMsg.Reactions {
 		reactedUser = append(reactedUser, e.Users...)
 	}
 
-	m := make(map[string]bool)
-	var delDuplication []string
-	var sendMsgUser []string
-
-	for _, e := range reactedUser {
-		if !m[e] {
-			m[e] = true
-			delDuplication = append(delDuplication, e)
-		}
-	}
-
-	for _, reacted := range delDuplication {
-		flg := false
-		var targetuser string
-		for _, target := range slackrn {
-			targetuser = target
-			if reacted == target {
-				flg = true
-			}
-		}
-
-		if !flg {
-			sendMsgUser = append(sendMsgUser, targetuser)
-		}
-	}
-
-	return sendMsgUser, nil
+	return reactedUser, nil
 }
